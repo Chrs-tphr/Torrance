3dda4b9bf662207d29e5f8e78bfc84ac4252bdb1
Christopher <cisensano@yahoo.com>
2017-03-23 12:29:40 -0700

    Updated eval from 100 to 1 for validateWorkOrderAssetAndCosting()

--- INCLUDES/INCLUDES_CUSTOM.js
+++ INCLUDES/INCLUDES_CUSTOM.js
@@ -2,7 +2,7 @@
 | Program	: INCLUDES_CUSTOM.js
 | Event		: N/A
 | Agency	: Torrance
-| Version	: 11.10.2016.13:11.pst
+| Version	: 03.23.2017.12:19.pst
 |
 | Usage		: Custom Script Include.  Insert custom EMSE Function below and they will be available to all master scripts
 |
@@ -15,6 +15,7 @@
 |			: 12/22/2016 - Updated assignCapToDept() to correct break error and added additional debugging, 
 |			: 12/22/2016 - Updated addConditionalAssessmentsForWaterValves() and addConditionalAssessmentsForHydrants() to set Status to "Scheduled"
 |			: 01/12/2017 - Updated assignCapToDept() to set assigned to staff to null when the department is assigned.
+|			: 03/23/2017 - Added validateWorkOrderAssetAndCosting() and getWorkOrderCostingTransactions() for validating on Status Update before.
 |
 /------------------------------------------------------------------------------------------------------*/
 
@@ -3765,3 +3766,100 @@ function assignCapToStaff(assignId){ // optional CapId
 	if (cdWrite.getSuccess()){ logDebug("Assigned CAP to " + assignId); }
 	else { logDebug("**ERROR writing capdetail : " + cdWrite.getErrorMessage()); return false; }
 }
+
+function getWorkOrderCostingTransactions(id1, id2, id3) {
+    aa.print("Enter getWorkOrderCostingTransactions()");
+
+    //aa.print("id1:"+ id1 + " id2:" + id2 + " id3:" + id3);
+
+    var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
+    var datastore = initialContext.lookup("java:/AA");
+    var connection = datastore.getConnection();
+
+    var sqlString = "";
+
+    sqlString += "SELECT * ";
+    sqlString += "FROM GWORK_ORDER_COSTING ";
+    sqlString += "WHERE ";
+    sqlString += "    SERV_PROV_CODE = ? ";
+    sqlString += "        AND ";
+    sqlString += "    B1_PER_ID1 = ? ";
+    sqlString += "        AND ";
+    sqlString += "    B1_PER_ID2 = ? ";
+    sqlString += "        AND ";
+    sqlString += "    B1_PER_ID3 = ? ";
+
+    //aa.print(sqlString);
+
+    var workOrderCostingTransactions = [];
+
+    var sqlStatement = connection.prepareStatement(sqlString);
+
+    sqlStatement.setString(1, aa.getServiceProviderCode());
+    sqlStatement.setString(2, id1);
+    sqlStatement.setString(3, id2);
+    sqlStatement.setString(4, id3);
+
+    var recordset = sqlStatement.executeQuery();
+
+    while (recordset.next()) {
+        var COST_SEQ_NBR = recordset.getLong("COST_SEQ_NBR");
+        workOrderCostingTransactions.push({ COST_SEQ_NBR: COST_SEQ_NBR });
+    }
+
+    sqlStatement.close();
+    connection.close();
+
+    var objectMapper = new org.codehaus.jackson.map.ObjectMapper();
+    //aa.print(objectMapper.writeValueAsString(workOrderCostingTransactions));
+
+    aa.print("Exit getWorkOrderCostingTransactions()");
+    return workOrderCostingTransactions;
+}
+
+function validateWorkOrderAssetAndCosting() {
+    aa.print("Enter validateWorkOrderAssetAndCosting.js");
+
+    //aa.print("capId: " + capId);
+
+    //aa.print("cap: " + cap);
+
+    var targetStatus = "Field Complete";
+    var updatedStatus = appStatus;
+
+    if (targetStatus == updatedStatus) {
+        aa.print("Begin if(){}");
+
+        var capModel = cap.getCapModel();
+        //aa.print("capModel:" + capModel);
+
+        var assetList = capModel.getAssetList();
+        //aa.print("assetList.length: " + assetList.toArray().length);
+
+        var workOrderCostingTransactions = getWorkOrderCostingTransactions(capId.ID1, capId.ID2, capId.ID3)
+        //aa.print("workOrderCostingTransactions: " + workOrderCostingTransactions.length);
+
+        if (assetList.length < 1 || workOrderCostingTransactions.length < 1) {
+            aa.print("Begin cancelling transaction");
+
+            aa.print("ScriptReturnCode: " + aa.env.getValue("ScriptReturnCode"));
+            aa.env.setValue("ScriptReturnCode", 1);
+            aa.print("ScriptReturnCode: " + aa.env.getValue("ScriptReturnCode"));
+
+            aa.print("ScriptReturnMessage: " + aa.env.getValue("ScriptReturnMessage"));
+            aa.env.setValue("ScriptReturnMessage", "This record must have one asset record and one costing record before status can be set to '" + targetStatus + "'");
+            aa.print("ScriptReturnMessage: " + aa.env.getValue("ScriptReturnMessage"));
+
+            aa.print("cancel: " + cancel);
+            cancel = true;
+            aa.print("cancel: " + cancel);
+
+            aa.print("End cancelling transaction");
+        }
+
+        aa.print("End if(){}");
+    }
+
+    aa.print("Exit validateWorkOrderAssetAndCosting.js");
+}
+
